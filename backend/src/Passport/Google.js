3b0cd440f62393843af4c73acfc6ff9661afa745
Bryan Urrutia <66961781+Bryan-Urrutia@users.noreply.github.com>
2024-02-22 19:28:37 -0300

    Update Google.js

--- backend/src/Passport/Google.js
+++ backend/src/Passport/Google.js
@@ -9,7 +9,7 @@ passport.use("google",
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "/google/callback",
+            callbackURL: process.env.CALLBACKURL,
         },
         async (accessToken, refreshToken, profile, done) => {
             const user = await User.findOne({
